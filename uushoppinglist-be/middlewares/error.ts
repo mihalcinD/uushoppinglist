87c74a1ae84f56fc1bffa669ac86db77a77e8221
Dávid Mihalčin <mihalcindavid@gmail.com>
2023-11-12 19:16:24 +0100

    feat(schemas): dtoIn

--- uushoppinglist-be/middlewares/error.ts
+++ uushoppinglist-be/middlewares/error.ts
@@ -2,7 +2,7 @@ import { Request, Response, NextFunction } from 'express';
 
 const ErrorHandler = (err: any, req: Request, res: Response, next: NextFunction) => {
 	const status = err.statusCode || 500;
-	const message = err.message || 'Something went wrong';
+	const message = err.message || 'Something went wrong :(';
 	res.status(status).json({
 		success: false,
 		status,
---
+++ uushoppinglist-be/schemas/lists.schema.ts
@@ -0,0 +1,47 @@
+import Ajv from 'ajv';
+import { identifierSchema } from './general.schema';
+
+const ajv = new Ajv();
+
+const createSchema = {
+	type: 'object',
+	properties: {
+		name: {
+			type: 'string',
+			minLength: 1,
+		},
+		memberIDs: {
+			type: 'array',
+			items: identifierSchema,
+		},
+		items: {
+			type: 'array',
+			items: {
+				type: 'string',
+				minLength: 1,
+			},
+		},
+	},
+	additionalProperties: false,
+	required: ['name', 'memberIDs', 'items'],
+};
+
+const updateSchema = {
+	type: 'object',
+	properties: {
+		name: {
+			type: 'string',
+			minLength: 1,
+		},
+		isArchived: {
+			type: 'boolean',
+		},
+	},
+	anyOf: [{ required: ['name'] }, { required: ['isArchived'] }],
+};
+
+export const listsSchema = {
+	createSchema: ajv.compile(createSchema),
+	updateSchema: ajv.compile(updateSchema),
+	identifierSchema: ajv.compile(identifierSchema),
+};
