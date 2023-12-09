0fcd17eec538da6805eab3aaad19d06a7a8e21a9
Dávid Mihalčin <mihalcindavid@gmail.com>
2023-12-09 15:17:44 +0100

    feat(jest): setup

---
+++ uushoppinglist-be/__tests__/list.test.ts
@@ -0,0 +1,15 @@
+describe('List', () => {
+	describe('Get lists', () => {
+		it('should return 200 OK', () => {
+			expect(1).toBe(1);
+		});
+	});
+});
+
+describe('List', () => {
+	describe('Get list', () => {
+		it('should return 200 OK', () => {
+			expect(1).toBe(1);
+		});
+	});
+});
